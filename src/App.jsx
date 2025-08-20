58c6e6b316bf7184e73599be83c93b139e9ea37f
Chun <mevius92201@gmail.com>
2025-08-20 15:31:27 +0800

    create simple site frame

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <main
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          fontSize: "1.5rem",
+        }}
+      >
+        百鬼御神籤
+      </main>
+    </>
+  );
+}
+
+export default App;
